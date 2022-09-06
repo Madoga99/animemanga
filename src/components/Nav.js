5d1969f883d3b606854a78922644c3ce704d7292
Madoga99 <111305071+Madoga99@users.noreply.github.com>
2022-09-06 12:18:58 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
   
     return (
       <nav>
-        <h1>RADIO CATOLICA</h1>
+        <h1>ANIME&MANGA</h1>
         <button
           className={libraryStatus ? "library-active" : ""}
           onClick={openLibraryHandler}
